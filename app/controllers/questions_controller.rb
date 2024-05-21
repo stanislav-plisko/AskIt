399bb95c31eebe44a3276297aa437d262a5a340d
stanislav <stas.plisko.99@mail.ru>
2024-05-21 21:20:56 +0300

    configured pagination, seeds, faker and custom markup

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,7 +13,7 @@ class QuestionsController < ApplicationController
     end
 
     def index
-        @questions = Question.all
+        @questions = Question.order(created_at: :desc).page params[:page]
     end
 
     def new 
@@ -32,7 +32,7 @@ class QuestionsController < ApplicationController
 
     def show
         @answer = @question.answers.build
-        @answers = @question.answers.order created_at: :desc
+        @answers = @question.answers.order(created_at: :desc).page(params[:page]).per(2)
         # Answer.where(question: @question).limit(2).order(created_at: :desc)
     end
 
